25215c0977cb07d9e22c0dde58fc426d87310523
Web-yue <1216363819@qq.com>
2019-06-22 22:08:59 +0800

    'axios请求练习'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -8,10 +8,18 @@
 <script>
   import Header from './components/Header'
   import Swiper from './components/HomeSwiper'
+  import axios from 'axios'
     export default {
         name: "Home",
       components:{
           Header,Swiper
+      },
+      created() {
+          axios.get('/api/data.json').then(res=>{
+            console.log(res)
+        }).catch(err=>{
+          console.log(err)
+          })
       }
     }
 </script>
