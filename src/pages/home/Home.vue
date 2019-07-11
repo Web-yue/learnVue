d27532ef7426c274a624f80ee2639ba41e26877f
Web-yue <1216363819@qq.com>
2019-07-11 11:16:07 +0800

    'keep-alive使用'

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-      <Header></Header>
-      <Swiper :swiperArr="swiperArr"></Swiper>
-      <Icons :iconsArr="iconsArr"></Icons>
-     </div>
+  <div>
+    <Header></Header>
+    <Swiper :swiperArr="swiperArr"></Swiper>
+    <Icons :iconsArr="iconsArr"></Icons>
+  </div>
 </template>
 
 <script>
@@ -11,32 +11,53 @@
   import Swiper from './components/HomeSwiper'
   import Icons from './components/Icons'
   import axios from 'axios'
+  import {mapState} from 'vuex'
+
   export default {
     name: "Home",
-    components:{
-      Header,Swiper,Icons
-      },
-    data(){
+    components: {
+      Header, Swiper, Icons
+    },
+    data() {
       return {
-        swiperArr:[],
-        iconsArr:[]
+        swiperArr: [],
+        iconsArr: [],
+        lastCity: ''
       }
     },
-      created() {
-          axios.get('/api/data.json').then(res=>{
-            if(res.data.res&&res.data.data){
-              const data=res.data.data;
-              this.swiperArr=data.swiperArr;
-              this.iconsArr=data.iconsArr;
+    computed: {
+      ...mapState(['city'])
+    },
+    methods:{
+      getData(){
+        axios.get('/api/data.json', {
+          params: {
+            city: this.city
+          }
+        }).then(res => {
+          if (res.data.res && res.data.data) {
+            const data = res.data.data;
+            this.swiperArr = data.swiperArr;
+            this.iconsArr = data.iconsArr;
 
-            }
-        }).catch(err=>{
+          }
+        }).catch(err => {
           console.log(err)
 
-          })
-
+        })
       }
+    },
+    mounted(){
+      this.getData();
+      this.lastCity=this.city
+    },
+   activated() { //keep-alive 生命周期函数 组件被重新加载的时候执行
+    if(this.lastCity!==this.city){ //解决keep-alive造成的ajax没有重新请求问题
+      this.getData();
+      this.lastCity=this.city
     }
+   }
+  }
 </script>
 
 <style scoped>
